f301cabd26da9b31ecf927ed4b1a9b60db050773
vixhnuchandran <vixhnuchandran@gmail.com>
2024-09-11 21:54:36 +0530

    Variables and mutability

---
+++ variables_and_mutability/target/debug/deps/variables-37d8d2eeee44b0b1.d
@@ -0,0 +1,5 @@
+/home/user/Rust/variables/target/debug/deps/libvariables-37d8d2eeee44b0b1.rmeta: src/main.rs
+
+/home/user/Rust/variables/target/debug/deps/variables-37d8d2eeee44b0b1.d: src/main.rs
+
+src/main.rs:
