07f7b2f6867b6e85780f8cd728aa126a32a33ee4
vixhnuchandran <vixhnuchandran@gmail.com>
2024-09-11 21:50:36 +0530

    Data types

---
+++ data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/home/user/Rust/data_types/target/debug/data_types: /home/user/Rust/data_types/src/main.rs
---
+++ data_types/target/debug/deps/data_types-bea3276a75bff3a2.d
@@ -0,0 +1,5 @@
+/home/user/Rust/data_types/target/debug/deps/data_types-bea3276a75bff3a2: src/main.rs
+
+/home/user/Rust/data_types/target/debug/deps/data_types-bea3276a75bff3a2.d: src/main.rs
+
+src/main.rs:
