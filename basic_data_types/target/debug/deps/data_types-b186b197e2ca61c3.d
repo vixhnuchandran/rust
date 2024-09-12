584a2eacb75db8abf58472fe9a1f1c73d5691a47
vixhnuchandran <vixhnuchandran@gmail.com>
2024-09-12 10:41:22 +0530

    Basic data types

---
+++ basic_data_types/target/debug/deps/data_types-b186b197e2ca61c3.d
@@ -0,0 +1,5 @@
+/home/user/Rust/basic_data_types/target/debug/deps/libdata_types-b186b197e2ca61c3.rmeta: src/main.rs
+
+/home/user/Rust/basic_data_types/target/debug/deps/data_types-b186b197e2ca61c3.d: src/main.rs
+
+src/main.rs:
---
+++ basic_data_types/target/debug/deps/data_types-d3aa928da453669d.d
@@ -0,0 +1,5 @@
+/home/user/Rust/basic_data_types/target/debug/deps/libdata_types-d3aa928da453669d.rmeta: src/main.rs
+
+/home/user/Rust/basic_data_types/target/debug/deps/data_types-d3aa928da453669d.d: src/main.rs
+
+src/main.rs:
