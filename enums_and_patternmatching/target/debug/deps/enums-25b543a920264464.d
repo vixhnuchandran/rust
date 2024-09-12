24ae3f8b11b1f0386b744744e2514a205e5facd2
vixhnuchandran <vixhnuchandran@gmail.com>
2024-09-12 10:41:35 +0530

    Enums and pattern matching

---
+++ enums_and_patternmatching/target/debug/deps/enums-25b543a920264464.d
@@ -0,0 +1,5 @@
+/home/user/Rust/enums_and_patternmatching/target/debug/deps/libenums-25b543a920264464.rmeta: src/main.rs
+
+/home/user/Rust/enums_and_patternmatching/target/debug/deps/enums-25b543a920264464.d: src/main.rs
+
+src/main.rs:
