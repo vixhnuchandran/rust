68348829458d95a6a240324f357c44f165aea81c
vixhnuchandran <vixhnuchandran@gmail.com>
2024-09-12 10:41:30 +0530

    Borrowing and references

---
+++ borrowing_and_references/target/debug/deps/borrowing_and_references-e6d2c79e1e4a2ea3.d
@@ -0,0 +1,5 @@
+/home/user/Rust/borrowing_and_references/target/debug/deps/libborrowing_and_references-e6d2c79e1e4a2ea3.rmeta: src/main.rs
+
+/home/user/Rust/borrowing_and_references/target/debug/deps/borrowing_and_references-e6d2c79e1e4a2ea3.d: src/main.rs
+
+src/main.rs:
