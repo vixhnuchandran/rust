6e048a6af29dad2374ddd862eb1a1156051f33f7
vixhnuchandran <vixhnuchandran@gmail.com>
2024-09-12 10:41:43 +0530

    Functions

---
+++ functions/target/debug/deps/function-6d3cc4e93ad5337d.d
@@ -0,0 +1,5 @@
+/home/user/Rust/functions/target/debug/deps/libfunction-6d3cc4e93ad5337d.rmeta: src/main.rs
+
+/home/user/Rust/functions/target/debug/deps/function-6d3cc4e93ad5337d.d: src/main.rs
+
+src/main.rs:
